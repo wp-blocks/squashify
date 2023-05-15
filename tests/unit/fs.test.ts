d070dd2d777dd27663ee4ae84874219a9abef6e7
Erik Golinelli <erik@codekraft.it>
2023-05-15 19:27:44 +0200

    fixes fs test

--- tests/unit/fs.test.ts
+++ tests/unit/fs.test.ts
@@ -34,12 +34,18 @@ describe('srcDirQuestion', () => {
 
 
 describe('getImageFormatsInFolder', () => {
+
+	beforeAll(() => {
+		if (!fs.existsSync('tests/images/empty'))
+			fs.mkdirSync('tests/images/empty');
+	})
+
 	it('should return an empty array when given an empty folder', () => {
 		expect(getImageFormatsInFolder('tests/images/empty')).toEqual([]);
 	});
 
 	it('should return an array of image formats for a folder containing images', () => {
-		expect(getImageFormatsInFolder('tests/images/test1')).toEqual(expect.arrayContaining(['.gif', '.jpg', '.png', '.svg',  '.tiff']));
+		expect(getImageFormatsInFolder('tests/images/test1')).toEqual(expect.arrayContaining(['.gif', '.jpg', '.png', '.svg', '.tiff']));
 		expect(getImageFormatsInFolder('tests/images/test2')).toEqual(expect.arrayContaining(['.jpg', '.png']));
 		expect(getImageFormatsInFolder('tests/images/test3')).toEqual(expect.arrayContaining(['.svg']));
 	});
