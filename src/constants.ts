e643d768b9d3412341c1562a63ff1354d47e963f
Erik Golinelli <erik@codekraft.it>
2024-03-24 12:16:40 +0100

    lint
    Took 22 seconds

--- src/constants.ts
+++ src/constants.ts
@@ -1,61 +1,65 @@
-import { type Choice } from 'prompts'
+import { type Choice } from "prompts";
+import { PluginConfig } from "svgo";
 
-export const defaultSrc = 'src/images'
-export const defaultDist = 'images'
-export const defaultConfigFile = '.squash'
+export const defaultSrc = "src/images";
+export const defaultDist = "images";
+export const defaultConfigFile = ".squash";
 
-export const compressors = ['avif', 'webp', 'mozjpeg', 'jpg', 'png'] as const
+export const compressors = ["avif", "webp", "mozjpeg", "jpg", "png"] as const;
 
-export type Compressor = typeof compressors[ number ] | undefined
+export type Compressor = (typeof compressors)[number] | undefined;
 
 export const inputFormats = [
-  '.jpg',
-  '.jpeg',
-  '.png',
-  '.webp',
-  '.avif',
-  '.tiff',
-  '.gif',
-  '.svg'
-] as const
+	".jpg",
+	".jpeg",
+	".png",
+	".webp",
+	".avif",
+	".tiff",
+	".gif",
+	".svg",
+];
 
-export type InputFormats = typeof inputFormats[ number ]
+export type InputFormats = (typeof inputFormats)[number];
 
 export const svgOptions: Choice[] = [
-  { title: 'CleanupAttrs', value: 'cleanupAttrs' },
-  { title: 'RemoveDoctype', value: 'removeDoctype' },
-  { title: 'RemoveXMLProcInst', value: 'removeXMLProcInst' },
-  { title: 'RemoveComments', value: 'removeComments' },
-  { title: 'RemoveMetadata', value: 'removeMetadata' },
-  { title: 'RemoveXMLNS', value: 'removeXMLNS' },
-  { title: 'RemoveEditorsNSData', value: 'removeEditorsNSData' },
-  { title: 'RemoveTitle', value: 'removeTitle' },
-  { title: 'RemoveDesc', value: 'removeDesc' },
-  { title: 'RemoveUselessDefs', value: 'removeUselessDefs' },
-  { title: 'RemoveEmptyAttrs', value: 'removeEmptyAttrs' },
-  { title: 'RemoveHiddenElems', value: 'removeHiddenElems' },
-  { title: 'RemoveEmptyContainers', value: 'removeEmptyContainers' },
-  { title: 'RemoveEmptyText', value: 'removeEmptyText' },
-  { title: 'RemoveUnusedNS', value: 'removeUnusedNS' },
-  { title: 'ConvertShapeToPath', value: 'convertShapeToPath' },
-  { title: 'SortAttrs', value: 'sortAttrs' },
-  { title: 'MergePaths', value: 'mergePaths' },
-  { title: 'SortDefsChildren', value: 'sortDefsChildren' },
-  { title: 'RemoveDimensions', value: 'removeDimensions' },
-  { title: 'RemoveStyleElement', value: 'removeStyleElement' },
-  { title: 'RemoveScriptElement', value: 'removeScriptElement' },
-  { title: 'InlineStyles', value: 'inlineStyles' },
-  { title: 'removeViewBox', value: 'removeViewBox' },
-  { title: 'removeElementsByAttr', value: 'removeElementsByAttr' },
-  { title: 'cleanupIDs', value: 'cleanupIds' },
-  { title: 'convertColors', value: 'convertColors' },
-  { title: 'removeRasterImages', value: 'removeRasterImages' },
-  {
-    title: 'removeUselessStrokeAndFill',
-    value: 'removeUselessStrokeAndFill'
-  },
-  {
-    title: 'removeNonInheritableGroupAttrs',
-    value: 'removeNonInheritableGroupAttrs'
-  }
-]
+	{ title: "CleanupAttrs", value: "cleanupAttrs" },
+	{ title: "RemoveDoctype", value: "removeDoctype" },
+	{ title: "RemoveXMLProcInst", value: "removeXMLProcInst" },
+	{ title: "RemoveComments", value: "removeComments" },
+	{ title: "RemoveMetadata", value: "removeMetadata" },
+	{ title: "RemoveXMLNS", value: "removeXMLNS" },
+	{ title: "RemoveEditorsNSData", value: "removeEditorsNSData" },
+	{ title: "RemoveTitle", value: "removeTitle" },
+	{ title: "RemoveDesc", value: "removeDesc" },
+	{ title: "RemoveUselessDefs", value: "removeUselessDefs" },
+	{ title: "RemoveEmptyAttrs", value: "removeEmptyAttrs" },
+	{ title: "RemoveHiddenElems", value: "removeHiddenElems" },
+	{ title: "RemoveEmptyContainers", value: "removeEmptyContainers" },
+	{ title: "RemoveEmptyText", value: "removeEmptyText" },
+	{ title: "RemoveUnusedNS", value: "removeUnusedNS" },
+	{ title: "ConvertShapeToPath", value: "convertShapeToPath" },
+	{ title: "SortAttrs", value: "sortAttrs" },
+	{ title: "MergePaths", value: "mergePaths" },
+	{ title: "SortDefsChildren", value: "sortDefsChildren" },
+	{ title: "RemoveDimensions", value: "removeDimensions" },
+	{ title: "RemoveStyleElement", value: "removeStyleElement" },
+	{ title: "RemoveScriptElement", value: "removeScriptElement" },
+	{ title: "InlineStyles", value: "inlineStyles" },
+	{ title: "removeViewBox", value: "removeViewBox" },
+	{ title: "removeElementsByAttr", value: "removeElementsByAttr" },
+	{ title: "cleanupIDs", value: "cleanupIds" },
+	{ title: "convertColors", value: "convertColors" },
+	{ title: "removeRasterImages", value: "removeRasterImages" },
+	{
+		title: "removeUselessStrokeAndFill",
+		value: "removeUselessStrokeAndFill",
+	},
+	{
+		title: "removeNonInheritableGroupAttrs",
+		value: "removeNonInheritableGroupAttrs",
+	},
+];
+
+export const defaultSvgoPlugins: PluginConfig[] = ["preset-default"];
+export const extModes = ["replace", "add"];
--- src/types.ts
+++ src/types.ts
@@ -1,27 +1,32 @@
-import { type Compressor, type InputFormats } from './constants'
-import {PluginConfig as SvgoPluginConfig} from "svgo";
+import { type Compressor, extModes, type InputFormats } from "./constants";
+import { PluginConfig as SvgoPluginConfig } from "svgo";
 
 /* These are types defining the options for compressing different types of files. */
 export type CompressionOptions = {
-	compress?: string
-	compressor?: Compressor
-	quality?: number
-	progressive?: boolean
-	plugins?: SvgoPluginConfig[]
-}
+	compress?: string;
+	compressor?: Compressor;
+	extMode?: ExtModes;
+	quality?: number;
+	progressive?: boolean;
+	plugins?: SvgoPluginConfig[];
+};
 
-export type CompressionOptionsMap = { [ key in InputFormats ]: CompressionOptions }
+export type CompressionOptionsMap = {
+	[key in InputFormats]?: CompressionOptions;
+};
 
+export type ExtModes = "add" | "replace";
 /* The `ScriptOptions` interface is defining the options that can be passed to a script. */
 export interface ScriptOptions {
-  srcDir: string
-  distDir: string
-  configFile?: string
-  verbose?: boolean
-  interactive?: boolean
-  compressionOptions?: Partial<CompressionOptionsMap>
+	srcDir: string;
+	distDir: string;
+	configFile?: string;
+	verbose?: boolean;
+	extMode: ExtModes;
+	interactive?: boolean;
+	compressionOptions: CompressionOptionsMap;
 }
 
 export type IniOptions = {
-	[ key: string ]: string | unknown;
-}
+	[key: string]: string | unknown;
+};
