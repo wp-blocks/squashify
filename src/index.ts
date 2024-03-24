b450a01173425e0b34699752641ebc4655f6b049
Erik Golinelli <erik@codekraft.it>
2024-03-24 12:17:32 +0100

    a better way to encode medias async
    Took 52 seconds

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 #!/usr/bin/env node
-import yargs from 'yargs'
+import yargs from "yargs";
 
-import { getCliOptions } from './args'
-import { convertImages } from './compression'
-import { getIniOptions } from './ini'
-import { getPromptOptions } from './promps'
+import { getCliOptions } from "./args";
+import { convertImages } from "./compression";
+import { getIniOptions } from "./ini";
+import { getPromptOptions } from "./prompts";
+import { defaultCompressionOptions } from "./utils";
+import { hideBin } from "yargs/helpers";
+import process from "process";
 
 /**
  * Prompts the user for the source and destination directories
@@ -12,32 +15,57 @@ import { getPromptOptions } from './promps'
  *
  * @returns Promise that resolves when the image conversion is complete
  */
-export default async function main (): Promise<unknown> {
-  // Get the cli options
-  let options = getCliOptions(yargs(process.argv.slice(2)))
+export default async function main(): Promise<unknown> {
+	// Get the cli options
+	let options = getCliOptions(yargs(hideBin(process.argv)));
 
-  // Get the options from the ini file
-  options = getIniOptions(options)
+	// Get the options from the ini file
+	options = getIniOptions(options);
 
-  // Prompt the user for the script options
-  if (options.interactive === true) {
-    options = await getPromptOptions(options)
-  }
+	// check for missing options
+	const missingOptions = ["srcDir", "distDir"].filter(
+		(option) => !options[option as keyof typeof options],
+	);
 
-  // Start the timer
-  const startTime = Date.now()
+	// Prompt the user for the script options
+	if (options.interactive === true || missingOptions.length > 0) {
+		options = await getPromptOptions(options);
+	}
 
-  // Then convert the images in the source directory
-  const res = await convertImages(options)
+	if (Object.keys(options.compressionOptions).length === 0) {
+		console.log(
+			"No compression options found, so we will use the default compression options",
+		);
+		options.compressionOptions = defaultCompressionOptions();
+	}
 
-  if (res) {
-    // Print the time elapsed in seconds to the console
-    console.log( `The end 🎉 - Time elapsed: ${(Date.now() - startTime) / 1000} seconds` )
+	// Print the options to the console
+	if (options.verbose) {
+		console.log("Options:", options);
+	}
 
-    return res
-  }
+	// Start the timer
+	const startTime = Date.now();
+
+	// Then convert the images in the source directory
+	const res = await convertImages(options);
+
+	if (res) {
+		res.forEach((result) => {
+			if (result.status !== "fulfilled") {
+				console.log("🔴 " + result.reason);
+			} else {
+				console.log("🟢 " + result.value);
+			}
+		});
+		// Print the time elapsed in seconds to the console
+		console.log(
+			`The end 🎉 - Time elapsed: ${(Date.now() - startTime) / 1000} seconds`,
+		);
+		return res;
+	}
 }
 
 main().catch((err) => {
-  console.error(err)
-})
+	console.error(err);
+});
