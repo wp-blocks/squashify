b6078b414a9e6376c012be60e9c1dbf1a4a692ea
Erik Golinelli <erik@codekraft.it>
2024-04-03 21:50:38 +0200

    updated types
    Took 19 seconds

--- src/types.ts
+++ src/types.ts
@@ -1,32 +1,56 @@
-import { type Compressor, extModes, type InputFormats } from "./constants";
+import { type Compressor, type InputFormats } from "./constants";
 import { PluginConfig as SvgoPluginConfig } from "svgo";
+import { ParsedPath } from "path";
+import { Path } from "glob";
 
-/* These are types defining the options for compressing different types of files. */
-export type CompressionOptions = {
-	compress?: string;
-	compressor?: Compressor;
-	extMode?: ExtModes;
-	quality?: number;
-	progressive?: boolean;
-	plugins?: SvgoPluginConfig[];
-};
+export type ExtModes = "add" | "replace";
+
+export type ResizeType =
+	| "contain"
+	| "cover"
+	| "fill"
+	| "inside"
+	| "outside"
+	| "none";
 
 export type CompressionOptionsMap = {
 	[key in InputFormats]?: CompressionOptions;
 };
 
-export type ExtModes = "add" | "replace";
+export type IniOptions = {
+	[key: string]: string | unknown;
+};
+
 /* The `ScriptOptions` interface is defining the options that can be passed to a script. */
 export interface ScriptOptions {
 	srcDir: string;
 	distDir: string;
 	configFile?: string;
 	verbose?: boolean;
-	extMode: ExtModes;
+	options: CompressionOption;
 	interactive?: boolean;
 	compressionOptions: CompressionOptionsMap;
 }
 
-export type IniOptions = {
-	[key: string]: string | unknown;
+export type CompressImagePaths = {
+	file: Path;
+	src: string;
+	dist: string;
+} & ParsedPath;
+
+export interface CompressionOption {
+	extMode: ExtModes;
+	maxSize?: number | undefined;
+	resizeType?: ResizeType;
+}
+
+/* These are types defining the options for compressing different types of files. */
+export type CompressionOptions = {
+	compress?: string;
+	compressor?: Compressor;
+	quality?: number;
+	progressive?: boolean;
+	plugins?: SvgoPluginConfig[];
+	paths?: CompressImagePaths;
+	options?: CompressionOption;
 };
