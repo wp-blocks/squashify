7c7d81e91e1756046234a5188545628d2f9cfdc1
Erik Golinelli <erik@codekraft.it>
2024-04-04 15:40:53 +0200

    splitting parse functions
    Took 10 minutes

---
+++ src/parseIni.ts
@@ -0,0 +1,33 @@
+import fs from "fs";
+import ini from "ini";
+import { type IniOptions } from "./types";
+import path from "path";
+
+/**
+ * This function reads and parses a configuration file to get compression settings and updates the
+ * script settings accordingly.
+ *
+ * for a script. It is of type `ScriptOptions`.
+ * @returns the updated `settings` object with values from the configuration file, or the original
+ * `settings` object if no configuration file is found or if there is an error parsing the configuration
+ * file.
+ * @param configFile
+ */
+export function getIniOptions(
+	configFile: string | undefined = ".squash",
+): IniOptions {
+	let iniOptions: IniOptions = {};
+
+	try {
+		// Get the compression settings in the configuration file
+		iniOptions = ini.parse(
+			fs.readFileSync(path.join(process.cwd(), configFile), "utf-8"),
+		);
+	} catch (err) {
+		console.log(
+			`🎃 Squashify: Cannot find a valid configuration or ${configFile} file does not exist.`,
+		);
+	}
+
+	return iniOptions;
+}
--- src/parseOptions.ts
+++ src/parseOptions.ts
@@ -1,12 +1,5 @@
-import fs from "fs";
-import ini from "ini";
+import { ExtModes, IniOptions, ResizeType, ScriptOptions } from "./types";
 import { Compressor, inputFormats } from "./constants";
-import {
-	ExtModes,
-	type IniOptions,
-	ResizeType,
-	type ScriptOptions,
-} from "./types";
 import {
 	getDefaultCompressor,
 	getJpgCompressionOptions,
@@ -14,36 +7,6 @@ import {
 	getSvgoPluginOptions,
 	logMessage,
 } from "./utils";
-import path from "path";
-
-/**
- * This function reads and parses a configuration file to get compression settings and updates the
- * script settings accordingly.
- *
- * for a script. It is of type `ScriptOptions`.
- * @returns the updated `settings` object with values from the configuration file, or the original
- * `settings` object if no configuration file is found or if there is an error parsing the configuration
- * file.
- * @param configFile
- */
-export function getIniOptions(
-	configFile: string | undefined = ".squash",
-): IniOptions {
-	let iniOptions: IniOptions = {};
-
-	try {
-		// Get the compression settings in the configuration file
-		iniOptions = ini.parse(
-			fs.readFileSync(path.join(process.cwd(), configFile), "utf-8"),
-		);
-	} catch (err) {
-		console.log(
-			`🎃 Squashify: Cannot find a valid configuration or ${configFile} file does not exist.`,
-		);
-	}
-
-	return iniOptions;
-}
 
 export function parseOptions(
 	options: ScriptOptions,
