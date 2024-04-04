4cb29c0387c53aacd254a8e71a190298e7c4abe8
Erik Golinelli <erik@codekraft.it>
2024-04-04 12:54:37 +0200

    init args update
    Took 12 seconds

--- src/ini.ts
+++ src/ini.ts
@@ -1,108 +1,109 @@
 import fs from "fs";
-
 import ini from "ini";
-
 import { Compressor, inputFormats } from "./constants";
 import {
-	type CompressionOptionsMap,
+	CompressionOptions,
 	ExtModes,
 	type IniOptions,
 	ResizeType,
 	type ScriptOptions,
 } from "./types";
 import {
-	getCompressor,
+	getDefaultCompressor,
 	getJpgCompressionOptions,
 	getQuality,
 	getSvgoPluginOptions,
 	logMessage,
 } from "./utils";
 import { PluginConfig as SvgoPluginConfig } from "svgo";
+import path from "path";
 
 /**
- * This function reads and parses a configuration file to get compression options and updates the
- * script options accordingly.
+ * This function reads and parses a configuration file to get compression settings and updates the
+ * script settings accordingly.
  *
- * @param {ScriptOptions} scriptOptions - The `options` parameter is an object that contains various options
  * for a script. It is of type `ScriptOptions`.
- * @returns the updated `options` object with values from the configuration file, or the original
- * `options` object if no configuration file is found or if there is an error parsing the configuration
+ * @returns the updated `settings` object with values from the configuration file, or the original
+ * `settings` object if no configuration file is found or if there is an error parsing the configuration
  * file.
+ * @param configFile
  */
-export function getIniOptions(scriptOptions: ScriptOptions): ScriptOptions {
-	let iniOptions: IniOptions;
-
-	if (!scriptOptions.configFile) {
-		console.log(
-			`🎃 Squashify: No ${scriptOptions.configFile} file found. Please read the https://github.com/wp-blocks/squashify to know more about!`,
-		);
-		return scriptOptions;
-	}
+export function getIniOptions(
+	configFile: string | undefined = ".squash",
+): IniOptions {
+	let iniOptions: IniOptions = {};
 
 	try {
-		// Get the compression options in the configuration file
+		// Get the compression settings in the configuration file
 		iniOptions = ini.parse(
-			fs.readFileSync(`./${scriptOptions.configFile}`, "utf-8"),
+			fs.readFileSync(path.join(process.cwd(), configFile), "utf-8"),
 		);
 	} catch (err) {
 		console.log(
-			`🎃 Squashify: Cannot find a valid configuration or ${scriptOptions.configFile} file does not exist.`,
+			`🎃 Squashify: Cannot find a valid configuration or ${configFile} file does not exist.`,
 		);
-		return scriptOptions;
 	}
 
-	if (Object.keys(iniOptions).length > 0) {
-		// the source and destination directories
-		scriptOptions.srcDir =
-			scriptOptions.srcDir || (iniOptions.path as { in?: string })?.in || "";
-		scriptOptions.distDir =
-			scriptOptions.distDir || (iniOptions.path as { out?: string })?.out || "";
+	return iniOptions;
+}
 
-		scriptOptions.options = {
-			// the ext format options
-			extMode:
-				scriptOptions.options.extMode ??
-				(iniOptions.extMode as ExtModes) ??
-				"replace",
-			// the resize options
-			resizeType:
-				scriptOptions.options.resizeType ??
-				(iniOptions.resizeType as ResizeType) ??
-				"none",
-			maxSize:
-				scriptOptions.options.maxSize ??
-				Number(iniOptions.maxSize) ??
-				undefined,
-		};
+export function parseOptions(
+	options: ScriptOptions,
+	iniOptions: IniOptions,
+): ScriptOptions {
+	// the source and destination directories
+	options.srcDir = options.srcDir || String(iniOptions?.in) || "";
+	options.distDir = options.distDir || String(iniOptions?.out) || "";
 
-		// parse known options about formats
-		scriptOptions.compressionOptions = scriptOptions.compressionOptions ?? {};
-		inputFormats
-			// then parse the options for each format
-			.forEach((format) => {
-				const currentIniOption = iniOptions[format] as {
-					compressor?: Compressor;
-					quality?: string;
-					progressive?: boolean;
-					plugins?: string;
-				};
+	options.options = {
+		// the ext format settings
+		extMode:
+			options.options.extMode ?? (iniOptions.extMode as ExtModes) ?? "replace",
+		// the resize settings
+		resizeType:
+			options.options.resizeType ??
+			(iniOptions.resizeType as ResizeType) ??
+			"none",
+		maxSize:
+			Number(options.options.maxSize) ??
+			Number(iniOptions.maxSize) ??
+			undefined,
+	};
 
-				(scriptOptions.compressionOptions as CompressionOptionsMap)[format] = {
-					compressor: getCompressor(currentIniOption?.compressor, format),
-					quality: getQuality(Number(currentIniOption?.quality), format),
-					progressive: getJpgCompressionOptions(
-						currentIniOption?.progressive,
-						format,
-					),
-					plugins: getSvgoPluginOptions(currentIniOption?.plugins, format) as
-						| SvgoPluginConfig[]
-						| undefined,
-				};
-			});
-	}
+	// parse known settings about formats
+	options.compressionOptions = { ...options.compressionOptions };
+
+	// parse the settings for all formats in the inputFormats array
+	inputFormats
+		// then parse the settings for each format
+		.forEach((format) => {
+			const currentIniOption = iniOptions[format] as {
+				compressor?: Compressor;
+				quality?: string;
+				progressive?: boolean;
+				plugins?: string;
+			};
+
+			options.compressionOptions[format] = {
+				compressor: getDefaultCompressor(currentIniOption?.compressor, format),
+				quality: getQuality(Number(currentIniOption?.quality), format),
+			};
+
+			if (format === ".svg") {
+				options.compressionOptions[format].plugins = getSvgoPluginOptions(
+					currentIniOption?.plugins,
+					format,
+				) as SvgoPluginConfig[] | undefined;
+			}
+
+			if (format === ".jpg") {
+				options.compressionOptions[format].progressive =
+					getJpgCompressionOptions(currentIniOption?.progressive, format);
+			}
+		});
 
 	logMessage(
-		`Configuration file loaded, options: ${JSON.stringify(scriptOptions)} ${scriptOptions.verbose}`,
+		`Configuration file loaded, options: ${JSON.stringify(options)} ${options.verbose}`,
 	);
-	return scriptOptions;
+	return options;
 }
