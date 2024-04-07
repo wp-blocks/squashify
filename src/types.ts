91b97080d75d813f0c52fb4758a428433c72fa63
Erik Golinelli <erik@codekraft.it>
2024-04-07 11:40:55 +0200

    linting types and constants
    Took 42 seconds

--- src/types.ts
+++ src/types.ts
@@ -1,64 +1,109 @@
 import { type Compressor, type InputFormats } from "./constants";
-import { PluginConfig as SvgoPluginConfig } from "svgo";
+import { Config as SvgoConfig } from "svgo";
 import { ParsedPath } from "path";
+import { OutputInfo } from "sharp";
 
-export type ExtModes = "add" | "replace";
+export const extModes = ["add", "replace"];
 
-export type ResizeType =
-	| "contain"
-	| "cover"
-	| "fill"
-	| "inside"
-	| "outside"
-	| "none";
+export type ExtMode = (typeof extModes)[number];
+
+export const resizeType = [
+  "contain",
+  "cover",
+  "fill",
+  "inside",
+  "outside",
+  "none",
+];
+
+export type ResizeType = (typeof resizeType)[number];
 
 export type CompressionOptionsMap = {
-	[key in InputFormats]: CompressionOptions;
+  [key in InputFormats]: CompressionOptions;
 };
 
-export type IniOptions = {
-	[key: string]: unknown;
+export interface GenericCompressionOptions {
+  Compressor?: Compressor;
+}
+
+export interface SVGCompressionOption extends SvgoConfig {}
+
+export interface JpegCompressionOption extends GenericCompressionOptions {
+  quality?: number;
+  progressive?: boolean;
+}
+
+export type CompressionOption =
+  | JpegCompressionOption
+  | SVGCompressionOption
+  | GenericCompressionOptions;
+
+/**
+ * The `SquashOptions` interface is defining the settings that can be passed with the `.squash` .ini file that defines the general Squashify script settings.
+ */
+export type SquashOptions = {
+  extMode?: ExtMode;
+  resizeType?: ResizeType;
+  maxSize?: number;
+  outMargin?: number;
+  background?: string;
 };
 
-/* The `ScriptOptions` interface is defining the settings that can be passed to a script. */
-export interface ScriptOptions {
-	srcDir: string;
-	distDir: string;
-	configFile: string;
-	verbose?: boolean;
-	interactive?: boolean;
-	options: CompressionOption;
-	compressionOptions: CompressionOptionsMap;
+export interface CliOptions {
+  srcDir: string;
+  distDir: string;
+  verbose?: boolean;
+  configFile?: string;
+  interactive?: boolean;
+  options?: SquashOptions;
+}
+
+/**
+ * The `ScriptOptions` interface is defining the settings that can be passed to a script.
+ */
+export interface ScriptOptions extends CliOptions {
+  configFile?: string;
+  verbose?: boolean;
+  interactive?: boolean;
+  options?: SquashOptions;
+  compressionOptions: CompressionOptionsMap;
+}
+
+export interface IniOptions extends ScriptOptions {
+  in: string;
+  out: string;
 }
 
 export type CompressImagePaths = {
-	srcDir: string;
-	distDir: string;
-	relativePath: string;
-	filePath: string;
-	srcPath: string;
-	distPath: string;
-	cwd: string;
+  srcDir: string;
+  distDir: string;
+  relativePath: string;
+  filePath: string;
+  srcPath: string;
+  distPath: string;
+  cwd: string;
 } & ParsedPath;
-
-export interface CompressionOption {
-	extMode: ExtModes;
-	maxSize?: number | undefined;
-	resizeType?: ResizeType;
-	outMargin?: number;
-	background?: string;
-	plugins?: SvgoPluginConfig[];
-}
 
 /* These are types defining the settings for compressing different types of files. */
 export type CompressionOptions = {
-	compressor?: Compressor;
-	quality?: number;
-	progressive?: boolean;
-	originalSize?: number;
+  compressor?: Compressor;
+  quality?: number;
+  progressive?: boolean;
 };
 
 export interface CompressionMeta extends CompressionOptions {
-	paths: CompressImagePaths;
-	options: CompressionOption;
+  paths: CompressImagePaths;
+  options?: SquashOptions;
+  background?: string;
+  outMargin?: number;
 }
+
+export type OutputData =
+  | OutputInfo
+  | {
+      originalSize: number;
+      size: number;
+    }
+  | {
+      copy: boolean;
+    };
