81d247c7e37457a17ed2eda72ef224f876f240a9
Erik Golinelli <erik@codekraft.it>
2024-04-07 11:42:49 +0200

    removes ini related stuff from parsing
    Took 32 seconds

--- src/parseOptions.ts
+++ src/parseOptions.ts
@@ -1,71 +1,42 @@
-import { ExtModes, IniOptions, ResizeType, ScriptOptions } from "./types";
-import { Compressor, inputFormats } from "./constants";
 import {
-	getDefaultCompressor,
-	getJpgCompressionOptions,
-	getQuality,
-	getSvgoPluginOptions,
-	logMessage,
-} from "./utils";
+  CliOptions,
+  ExtMode,
+  IniOptions,
+  ResizeType,
+  ScriptOptions,
+} from "./types";
+import { logMessage } from "./utils";
 
 export function parseOptions(
-	settings: ScriptOptions,
-	iniOptions: IniOptions,
+  settings: CliOptions,
+  iniOptions: IniOptions | null = null,
 ): ScriptOptions {
-	// the source and destination directories
-	settings.srcDir = settings.srcDir || String(iniOptions?.in) || "";
-	settings.distDir = settings.distDir || String(iniOptions?.out) || "";
+  const newSettings: Partial<ScriptOptions> = {};
 
-	const { resizeType, maxSize }: { resizeType?: ResizeType; maxSize?: number } =
-		iniOptions.options ?? {
-			resizeType: "none",
-			maxSize: undefined,
-		};
+  // the source and destination directories
+  newSettings.srcDir = settings.srcDir || String(iniOptions?.in) || "";
+  newSettings.distDir = settings.distDir || String(iniOptions?.out) || "";
+  newSettings.verbose = settings.verbose || false;
+  newSettings.interactive = settings.interactive || false;
 
-	settings.options = {
-		// the ext format settings
-		extMode: (iniOptions.extMode as ExtModes) ?? "replace",
-		// the resize settings
-		resizeType: resizeType ?? "none",
-		maxSize: Number(maxSize) ?? undefined,
-	};
+  newSettings.options = iniOptions
+    ? {
+        // the ext format settings
+        extMode: (iniOptions.options?.extMode as ExtMode) ?? "replace",
+        // the resize settings
+        resizeType: (iniOptions.options?.resizeType as ResizeType) ?? "none",
+        // the maximum image size in pixels
+        maxSize: Number(iniOptions.options?.maxSize) ?? undefined,
+      }
+    : undefined;
 
-	// parse known settings about formats
-	settings.compressionOptions = { ...settings.compressionOptions };
+  // parse known settings about formats
+  newSettings.compressionOptions = iniOptions
+    ? { ...iniOptions.compressionOptions }
+    : undefined;
 
-	// parse the settings for all formats in the inputFormats array
-	inputFormats
-		// then parse the settings for each format
-		.forEach((format) => {
-			const currentIniOption = iniOptions[format] as Record<string, string>;
-
-			console.log(currentIniOption);
-
-			settings.compressionOptions[format] = {
-				compressor: getDefaultCompressor(
-					currentIniOption?.compressor,
-					format,
-				) as Compressor,
-				quality: getQuality(
-					Number(currentIniOption?.quality),
-					format,
-				) as number,
-			};
-
-			if (format === ".svg") {
-				settings.options.plugins = getSvgoPluginOptions(
-					currentIniOption?.plugins.split(","),
-				);
-			}
-
-			if (format === ".jpg" || format === ".jpeg") {
-				settings.compressionOptions[format].progressive =
-					getJpgCompressionOptions(Boolean(currentIniOption?.progressive));
-			}
-		});
-
-	logMessage(
-		`Configuration file loaded, options: ${JSON.stringify(settings)} ${settings.verbose}`,
-	);
-	return settings;
+  logMessage(
+    `Configuration file loaded, options: ${JSON.stringify(settings)} ${newSettings.verbose}`,
+  );
+  return newSettings as ScriptOptions;
 }
